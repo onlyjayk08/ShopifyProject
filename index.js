23db776551781b8cb5c83a42de0403ac24082029
Jay Patel <onlyjayk08@gmail.com>
2021-07-19 09:22:55 -0400

    make it better

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const router = require('./server/routes/routes');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+app.use(fileUpload());
+app.use(bodyParser.json());
+app.use(router);
+app.use(cors());
+
+dotenv.config({path: './config/config.env'});
+
+mongoose.connect(process.env.DataBase, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+    console.log("Error: " + err);
+});
+
+db.on("connected", (err, res) => {
+    if (err){
+        console.log("Error: " + err);
+    }
+    else{
+        console.log("Mongoose is connected");
+    }
+});
+
+app.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT}`));
+
