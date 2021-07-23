0a0e6e2eef6f810d4a12cd3752d12ed6998e4ed3
onlyjayk08 <86417009+onlyjayk08@users.noreply.github.com>
2021-07-23 00:18:58 -0400

    minor fix

--- index.js
+++ index.js
@@ -35,12 +35,12 @@ db.on("connected", (err, res) => {
     }
 });
 
-// if (process.env.NODE_ENV == "production") {
-//     // set static folder
-//     app.use(express.static("client/build"));
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve("./", "client", "build", "index.html"));
-//     });
-// }
+if (process.env.NODE_ENV == "production") {
+    // set static folder
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve("./", "client", "build", "index.html"));
+    });
+}
 
 app.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT}`));
